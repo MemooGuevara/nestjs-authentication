90e19565a84329d2e7bd21e7bc07d3a661a1f558
MemooGuevara <memooguevara@gmail.com>
2021-05-29 22:45:35 -0500

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { Configuration } from './config/config.keys'
 import { DatabaseModule } from './database/database.module'
+import { UserModule } from './modules/user/user.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    DatabaseModule
+    DatabaseModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService]
